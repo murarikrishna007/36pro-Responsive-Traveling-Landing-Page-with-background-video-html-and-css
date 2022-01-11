05f9ad6137d2b1b2bc1588a850a0ee456db55610
murarikrishna007 <murarikrishna0071994@gmail.com>
2022-01-11 10:26:58 +0530

    responsive traveling page build in html and css

---
+++ script.js
@@ -0,0 +1,10 @@
+// console.log("hello krishna");
+
+const menuToggle = document.querySelector(".toggle");
+const showCase = document.querySelector(".showcase");
+
+
+menuToggle.addEventListener("click", ()=>{
+    menuToggle.classList.toggle("active");
+    showCase.classList.toggle("active");
+})
